824794e310cc3686e513c21bd3295748a0dc7a86
TITHDAVID-RA <tithdavidra2457@gmail.com>
2025-06-19 16:54:27 +0700

    Initial

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,27 +1,13 @@
-import { Component } from '@angular/core';
-import Swiper from 'swiper';
-import 'swiper/css';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 
 @Component({
   selector: 'app-home-page',
   imports: [],
   templateUrl: './home-page.html',
-  styleUrl: './home-page.css'
+  styleUrl: './home-page.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePage {
-  swiper: Swiper;
 
-  constructor() {
-    this.swiper = new Swiper('.swiper-container', {
-      loop: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-      }
-    });
-  }
 }
